e7fafd0b5e963a44287cf53c1404a5709935dc6b
annametelskaya <annam.2620@gmail.com>
2018-10-13 17:04:21 +0300

    update lab4

--- unittests/src/lab4/testing/tests.java
+++ unittests/src/lab4/testing/tests.java
@@ -16,46 +16,70 @@ public class tests {
         Assert.assertFalse(Triangle.getTriangle(7, 3, 2));
     }
 
-    /*3. build a triangle with zero side*/
-    @Test
-    public void buildTriangleWithZeroSideShouldReturnFalse() {
-        Assert.assertFalse(Triangle.getTriangle(3, 0, 5));
-    }
-
-    /*4. build a triangle with negative side*/
-    @Test
-    public void buildTriangleWithNegativeSideShouldReturnFalse() {
-        Assert.assertFalse(Triangle.getTriangle(-1, 4, 5));
-    }
-
-    /*5. build isosceles triangle*/
+    /*3. build isosceles triangle*/
     @Test
     public void buildIsoscelesTriangleShouldReturnTrue() {
         Assert.assertTrue(Triangle.getTriangle(4, 4, 3));
     }
 
-    /*6. build triangle when sum of two equal side is equal to third side*/
+    /*4. build triangle when sum of two equal side is equal to third side*/
     @Test
     public void buildTriangleWhenSumOfTwoEqualSidesIsEqualToThirdShouldReturnFalse() {
         Assert.assertFalse(Triangle.getTriangle(3, 3, 6));
     }
 
-    /*7. build triangle when sum of two equal side is less than third side*/
+    /*5. build triangle when sum of two equal side is less than third side*/
     @Test
     public void buildTriangleWhenSumOfTwoEqualSidesIsLessThanThirdShouldReturnFalse() {
         Assert.assertFalse(Triangle.getTriangle(3, 3, 7));
     }
 
-    /*8. build right triangle*/
+    /*6. build right triangle*/
     @Test
     public void buildRightTriangleShouldReturnTrue() {
         Assert.assertTrue(Triangle.getTriangle(3, 4, 5));
     }
 
-    /*9. build equilateral triangle*/
+    /*7. build equilateral triangle*/
     @Test
     public void buildEquilateralTriangleShouldReturnTrue() {
         Assert.assertTrue(Triangle.getTriangle(3, 3, 3));
+    }
+
+    /*8. build a triangle when first side is 0*/
+    @Test
+    public void buildTriangleWhenFirstSideIsZeroShouldReturnFalse() {
+        Assert.assertFalse(Triangle.getTriangle(0, 8, 5));
+    }
+
+    /*9. build a triangle when second side is 0*/
+    @Test
+    public void buildTriangleWhenSecondSideIsZeroShouldReturnFalse() {
+        Assert.assertFalse(Triangle.getTriangle(3, 0, 5));
+    }
+
+    /*10. build a triangle when third side is 0*/
+    @Test
+    public void buildTriangleWhenThirdSideIsZeroShouldReturnFalse() {
+        Assert.assertFalse(Triangle.getTriangle(3, 5, 0));
+    }
+
+    /*11. build a triangle when first side is negative*/
+    @Test
+    public void buildTriangleWhenFirstSideIsNegativeShouldReturnFalse() {
+        Assert.assertFalse(Triangle.getTriangle(-1, 4, 5));
+    }
+
+    /*12. build a triangle when second side is negative*/
+    @Test
+    public void buildTriangleWhenSecondSideIsNegativeShouldReturnFalse() {
+        Assert.assertFalse(Triangle.getTriangle(3, -2, 5));
+    }
+
+    /*13. build a triangle when third side is negative*/
+    @Test
+    public void buildTriangleWhenThirdSideIsNegativeShouldReturnFalse() {
+        Assert.assertFalse(Triangle.getTriangle(3, 4, -2));
     }
 
 }
