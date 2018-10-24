92496e68d2a525a4cfe8f1c96fbb1f555609be0c
annametelskaya <annam.2620@gmail.com>
2018-10-24 22:12:11 +0300

    fix problems

--- userinterfacetests/src/main/java/lab5/testing/Tests.java
+++ userinterfacetests/src/main/java/lab5/testing/Tests.java
@@ -18,7 +18,6 @@ public class Tests {
 
     public void fillAllFields(WebDriver driver, WebDriverWait wait) throws InterruptedException {
         driver.get("https://www.airbaltic.com/en-BY/index");
-        Thread.sleep(5000);
         WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("input-container")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
         element.click();
@@ -27,7 +26,6 @@ public class Tests {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("date-field-wrap"))).click();
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("available"))).get(4).click();
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("available"))).get(1).click();
-        Thread.sleep(5000);
     }
 
     @Test
@@ -40,7 +38,6 @@ public class Tests {
         fillAllFields(driver, wait);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("date-field-wrap"))).click();
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("direct"))).get(10).click();
-        Thread.sleep(5000);
         String realError = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("form-errors"))).getText();
         String expectedError = "The date of the inbound flight cannot be earlier than the date of the outbound flight. Please adjust your selection.";
         driver.quit();
@@ -55,12 +52,10 @@ public class Tests {
         WebDriver driver = new ChromeDriver(options);
         WebDriverWait wait = new WebDriverWait(driver, 10);
         driver.get("https://www.airbaltic.com/en-BY/index");
-        Thread.sleep(5000);
         WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("input-container")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("btn"))).submit();
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("btn"))).submit();
-        Thread.sleep(5000);
         String realError = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("form-errors"))).getText();
         String expectedError = "Please select the departure date.\n" +
                 "Please select the return date.\n" +
@@ -81,7 +76,6 @@ public class Tests {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("pax-selector-block"))).click();
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[2]/div[2]/div[1]/div[2]/div/form/div[4]/div[2]/div[2]/div/div[2]/div[5]/span[2]"))).click();
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[2]/div[2]/div[1]/div[2]/div/form/div[4]/div[2]/div[2]/div/div[2]/div[5]/span[2]"))).click();
-        Thread.sleep(5000);
         String realError = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("form-errors"))).getText();
         String expectedError = "The number of infants can not be higher than the number of adults. Only an adult can accompany an infant.";
         driver.quit();
