f7a008baafbea205f049ce52fe1952ead425eb40
annametelskaya <annam.2620@gmail.com>
2018-11-09 13:07:57 +0300

    fix method

--- unittests/src/main/java/by/bsu/metelskaya/Triangle.java
+++ unittests/src/main/java/by/bsu/metelskaya/Triangle.java
@@ -14,9 +14,6 @@ public class Triangle {
     }
 
     boolean getTriangle() {
-        if (!(a > 0 || b > 0 || c > 0)) {
-            return false;
-        }
         return a + b > c && a + c > b && b + c > a;
     }
 }
