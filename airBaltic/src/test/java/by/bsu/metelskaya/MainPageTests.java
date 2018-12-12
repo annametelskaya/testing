c1387711aff94326047d144542c1560bdfddebe8
annametelskaya <annam.2620@gmail.com>
2018-12-12 19:55:31 +0300

    update chromedriver options

--- airBaltic/src/test/java/by/bsu/metelskaya/MainPageTests.java
+++ airBaltic/src/test/java/by/bsu/metelskaya/MainPageTests.java
@@ -17,10 +17,7 @@ public class MainPageTests {
     public void openPage() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--headless");
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
         driver = new ChromeDriver(options);
