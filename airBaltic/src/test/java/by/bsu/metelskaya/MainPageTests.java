0a9b1e5a91d80079763c18116f235e8b6b7d071a
annametelskaya <annam.2620@gmail.com>
2018-12-12 19:45:09 +0300

    fix chromedriver options

--- airBaltic/src/test/java/by/bsu/metelskaya/MainPageTests.java
+++ airBaltic/src/test/java/by/bsu/metelskaya/MainPageTests.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class MainPageTests {
     WebDriver driver;
@@ -15,7 +16,14 @@ public class MainPageTests {
     @Before
     public void openPage() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        driver = new ChromeDriver(options);
         driver.get("https://www.airbaltic.com/en-BY/index");
         steps = new MainPageSteps(driver);
     }
